a4513554f9e83ceca440509e9baeade52b96f8f6
tuanit168 <tuanit168@gmail.com>
2022-02-24 23:36:23 +0700

    Basic Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Book keeper</h1>
+      <nav style={{borderBottom: '1px solid', paddingBottom: '1rem'}}>
+        <Link to='/invoices'>Invoices</Link> |{' '}
+        <Link to='/expenses'>Expenses</Link>
+      </nav>
     </div>
   );
 }
