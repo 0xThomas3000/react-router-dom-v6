4dfb7b159e210355977107d4d6f39d470f73b751
tuanit168 <tuanit168@gmail.com>
2022-02-25 16:55:18 +0700

    Navigate when there's no invoice shown in the url

--- src/pages/Invoice.tsx
+++ src/pages/Invoice.tsx
@@ -1,20 +1,27 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import { invoices } from '../data/invoices';
 
 const Invoice = () => {
   const params = useParams<{ invoiceId: string }>();
+  const navigate = useNavigate();
 
   const invoice = invoices.find(
     invoice => invoice.id.toString() === params.invoiceId
   );
 
+  useEffect(() => {
+    if (!invoice) navigate('/invoices')
+  }, [invoice, navigate]);
+
   return (
     <h2 style={{ padding: '1rem' }}>
-      {
+      {/* {
         invoice 
           ? `Invoice #${invoice?.id} for ${invoice?.name}` 
           : 'No Invoice found!!!'
-      }
+      } */}
+      Invoice #{invoice?.id} for {invoice?.name}
     </h2>
   );
 }
