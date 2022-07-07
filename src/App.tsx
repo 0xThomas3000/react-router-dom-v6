1ba7ba08c672e70ea6dd50cb80aa555e54754ac7
tuanit168 <tuanit168@gmail.com>
2022-07-07 09:32:38 +0700

    Reseting the whole projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Book keeper</h1>
-      <nav style={{borderBottom: '1px solid', paddingBottom: '1rem'}}>
-        <Link to='invoices'>Invoices</Link> |{' '}
-        <Link to='expenses'>Expenses</Link>
-      </nav>
-      <Outlet />
+    <div>
+      Hello world
     </div>
   );
 }
